24efdd3a04de35fc99d28ad8bea71855d65b6fe0
Ben Wheeler <wheeler.benjamin@gmail.com>
2015-06-01 16:55:35 -0400

    Initial Commit

---
+++ FishbowlTests/FishbowlTests.swift
@@ -0,0 +1,36 @@
+//
+//  FishbowlTests.swift
+//  FishbowlTests
+//
+//  Created by Benjamin Wheeler on 6/1/15.
+//  Copyright (c) 2015 wheeler. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FishbowlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
